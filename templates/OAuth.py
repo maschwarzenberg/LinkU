f45cb8475ea10704b5ca6f8ab58b3a9251c15b52
maschwarzenberg <167848561+maschwarzenberg@users.noreply.github.com>
2024-05-07 22:47:53 -0700

    Add files via upload

---
+++ templates/OAuth.py
@@ -0,0 +1,15 @@
+redirectURI = "https://student-linku.com"
+URLENCODE(redirectURI) = "https%3A%2F%2Fstudent-linku.com"
+clientID = "77jbo7kk3f4e6o"
+clientSecret = "VKzKStpp9DnOuVnj"
+
+# Authorization Endpoint (Browser)
+scope = "r_liteprofile"
+
+access_token = AQVbfFOLp_PbUQ4_ezu4Gc0qeTGvS4NysQv1mVnPNQgZkdVYkFqmq4w_ghLCbQz1oBwZ7o4ciqye2ANEwkalZrwcDHMZ6b1cZyUqTKfAOrHKQTikzw5I4e_aZQcBbQ1dGrGrS3JXe33B_pe4HavaFmG3P-qI1GhdClSeJRRYLe36_ZUs6Tgd6XSe2Cu4tvHV4rKiW7iSyKlL2TC69NhBXr1xqvr6oqEmWQM4qhfygGmzC2rbnvrIfSwxGS9JdR4NWw2mvcsKuXz_ee-FadOvXNDHCMsNaXRMXRkUK0mgBYnG7amiieRdRgU_bolRzU4En3Gbi1YoeGoS0Q9hN8lrTAsnEGdmdA
+
+https://www.linkedin.com/oauth/v2/authorization?response_type=code&state=987654321&scope=r_liteprofile&client_id=77jbo7kk3f4e6o&redirect_uri=https%3A%2F%2Fstudent-linku.com
+
+https://www.linkedin.com/oauth/v2/authorization?response_type=code&state=987654321&scope=r_liteprofile&client_id=77jbo7kk3f4e6o&redirect_uri=https%3A%2F%2Fstudent-linku.com
+
+access_token
